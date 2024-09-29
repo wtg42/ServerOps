596ac1a9660aa745b7489bd11f1d37ff62d90637
weitingshih <weitingshih@softnext-inc.com>
2024-09-29 19:47:22 +0800

    feat: 建立系統架構

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ func main() {
 	// 使用新的路由匹配方式來處理 WebSocket 請求
 	mux.HandleFunc("/ws", handleWebSocket)
 
+	// 設定靜態文件路由，提供 dist 目錄中的文件
+	fs := http.FileServer(http.Dir("./dist")) // dist 是 Astro 編譯後的目錄
+	mux.Handle("/", fs)                       // 根目錄將提供靜態文件
+
 	// 啟動伺服器
 	log.Println("Server started on :8080")
 	err := http.ListenAndServe(":8080", mux)
