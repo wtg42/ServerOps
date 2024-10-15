830b9b71afa36fe8911203ef36bc5a49068a2236
weitingshih <weitingshih@softnext-inc.com>
2024-10-16 02:12:37 +0800

    feat: 基礎架設建構

--- sshserver/sshserver.go
+++ sshserver/sshserver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"log"
@@ -27,11 +28,12 @@ func main() {
 			switch cmd[0] {
 			case "logs":
 				// TODO: 這段改成使用 goroutine + StdoutPipe 持續讀取 tail -f
-				out, err := exec.Command("date").Output()
-				if err != nil {
-					io.WriteString(s, "Failed to run date command.\n")
-				}
-				s.Write(out)
+				fetchLogs()
+				// out, err := exec.Command("date").Output()
+				// if err != nil {
+				// 	io.WriteString(s, "Failed to run date command.\n")
+				// }
+				// s.Write(out)
 			default:
 				io.WriteString(s, cmd[0]+" <- Unknown command.\n")
 			}
@@ -43,5 +45,33 @@ func main() {
 	err := ssh.ListenAndServe(":2222", nil)
 	if err != nil {
 		log.Fatal("Failed to start SSH server: ", err)
+	}
+}
+
+func fetchLogs() {
+	cmd := exec.Command("tail", "-f", "/var/log/php.log", "/var/log/apache/error_log")
+	stdout, err := cmd.StdoutPipe()
+
+	if err != nil {
+		log.Fatalf("Error getting stdout pipe: %v", err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("Error starting command: %v", err)
+	}
+
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	// 處理讀取過程中的錯誤
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error reading stdout: %v", err)
+	}
+
+	// 等待命令結束
+	if err := cmd.Wait(); err != nil {
+		log.Fatalf("Command finished with error: %v", err)
 	}
 }
