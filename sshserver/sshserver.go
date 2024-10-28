92ffce9cfc02409c6b4f25bb5be3eaded06d641b
wtg42 <weiting.shi1982@gmail.com>
2024-10-28 16:07:52 +0800

    feat(sshserver): Add graceful shutdown function.

--- sshserver/sshserver.go
+++ sshserver/sshserver.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"io"
 	"log"
+	"os"
 	"os/exec"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 
 	"github.com/gliderlabs/ssh"
 )
 
+var wg sync.WaitGroup // 用於等待所有 goroutine 完成
+
 // SSH Server
 func main() {
+	const sshport string = ":2222"
+	sshServ := &ssh.Server{
+		Addr: sshport,
+	}
+
 	// 啟動 SSH 伺服器，指定連接的處理程序
-	ssh.Handle(func(s ssh.Session) {
+	sshServ.Handle(func(s ssh.Session) {
+		// Track session start and end.
+		wg.Add(1)
+		defer wg.Done()
 		// 取得使用者名稱
 		user := s.User()
 
@@ -40,19 +56,38 @@ func main() {
 		}
 	})
 
-	var sshport string = ":2222"
-	// 啟動伺服器，監聽 2222 埠口
-	log.Printf("Starting SSH server on port %s...", sshport)
-	err := ssh.ListenAndServe(sshport, nil)
-	if err != nil {
-		log.Fatal("Failed to start SSH server: ", err)
+	// Start up the SSH server, then listen for a signal.
+	go func() {
+		log.Printf("Starting SSH server on port %s...", sshport)
+		err := sshServ.ListenAndServe()
+		if err != nil {
+			log.Fatal("Failed to start SSH server: ", err)
+		}
+	}()
+
+	// Create a signal channel and listen for SIGINT and SIGTERM. e.g. Ctrl+C
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-sigc
+	log.Printf("Received signal %s: shutting down ssh server...\n", sig)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := sshServ.Shutdown(ctx); err != nil {
+		log.Fatalf("Failed to gracefully shutdown SSH server: %v", err)
 	}
+
+	wg.Wait()
+	log.Printf("SSH server gracefully stoped.")
 }
 
 // fetchLogs streams logs from
 // "/var/log/php.log" and "/var/log/apache/error_log".
 // Use io.WriteString() to return data.
 func fetchLogs(s ssh.Session) {
+	wg.Add(1)
+	defer wg.Done()
+
 	args := []string{
 		"tail",
 		"-f",
@@ -71,18 +106,23 @@ func fetchLogs(s ssh.Session) {
 		log.Fatalf("Error starting command: %v", err)
 	}
 
-	scanner := bufio.NewScanner(stdout)
-	for scanner.Scan() {
-		// 輸出當前 buffer 中的資料
-		io.WriteString(s, scanner.Text()+"\n")
-	}
+	// The tail -f cmd will block the current goroutine,
+	// so we need to create a goroutine to read stdout.
+	go func() {
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			// 輸出當前 buffer 中的資料
+			io.WriteString(s, scanner.Text()+"\n")
+		}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatalf("Error reading stdout: %s", err)
-	}
+		if err := scanner.Err(); err != nil {
+			log.Fatalf("Error reading stdout: %s", err)
+		}
+	}()
 
-	// 等待命令結束
-	if err := cmd.Wait(); err != nil {
-		log.Fatalf("Command finished with error: %s", err)
+	// If session is closed, kill the process.
+	<-s.Context().Done()
+	if cmd.Process != nil {
+		cmd.Process.Kill()
 	}
 }
